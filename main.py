27dfbb755fafa8a910a3b38cfd12aaa2652e3b1a
danpinkB <belozarovichdanila08@gmail.com>
2023-05-26 23:36:56 +0200

    bla

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import collections
 import json
 import pathlib
 import tempfile
@@ -5,7 +6,7 @@ import threading
 import time
 import zipfile
 from datetime import datetime
-from typing import Optional, List, Set, Dict
+from typing import Optional, Set, Dict, Tuple
 
 import requests
 import sqlite3
@@ -17,92 +18,111 @@ CONCRETE_ARTICLE_URL = "https://www.binance.com/en/news/flash/"
 
 
 class SqlliteConnector:
-    conn: sqlite3.Connection
-    cursor: sqlite3.Cursor
-
     def __init__(self):
         self.conn = sqlite3.connect("./articles.db")
         self.cursor = self.conn.cursor()
         self.conn.execute('''
-        create table if not exists articles (id INTEGER PRIMARY KEY)
+        create table if not exists articles (
+            id INTEGER PRIMARY KEY, 
+            posted_date DATE, 
+            is_parsed INTEGER
+        )
         ''')
+        self.articles = dict()
+        self.articles_ids = list()
+        self.cursor.execute("select * from articles order by posted_date")
+        for row in self.cursor.fetchall():
+            id_ = row[0]
+            self.articles[id_] = (datetime.strptime(row[1], "%Y-%m-%dT%H:%M:%S"), row[2])
+            self.articles_ids.append(id_)
 
-    def insert_article(self, article: int):
-        self.cursor.execute("insert into articles (id) values (?)", (article,))
+    # for context manager
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type_, value, traceback):
         self.conn.commit()
 
-    def is_parsed(self, article: int):
-        self.conn.execute("select * from articles where id = ?", (article,))
-        row = self.cursor.fetchone()
-        return row is not None
+    # for context manager
+
+    def insert_article(self, article: int, posted_date: datetime, is_parsed: int)->None:
+        self.cursor.execute("insert into articles (id,posted_date,is_parsed) values (?,?,?)",
+                            (article, posted_date.isoformat(), is_parsed))
+        self.articles[article] = (posted_date, is_parsed)
+        self.articles_ids.insert(0, article)
+
+    def set_parsed(self, article: int) -> None:
+        self.cursor.execute("update articles set is_parsed = 1 where id = ?", (article,))
+
+    def is_parsed(self, article: int) -> bool:
+        return self.articles.get(article)[1] == 1
+
+    def get_size(self) -> int:
+        return len(self.articles_ids)
+
+    def has_article(self, article: int) -> bool:
+        return self.articles.get(article) is not None
+
+    def __getitem__(self, item) -> int:
+        return self.articles_ids[item]
+
+    def get_article_info(self, article: int) -> Tuple:
+        return self.articles.get(article)
 
 
 class RequestInspector:
-    lock: threading.Lock
-    hour_limit: int
-    minute_limit: int
-    delta: int
-    last_parsed_time: float
-    headers: dict
-
-    def __init__(self, hour_limit=720000, headers: dict = None):
+    def __init__(self, req_hour_rate=720000, headers: Dict = None):
         if headers is None:
-            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0'}
-        self.hour_limit = hour_limit
+            headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0'}
+        self.hour_limit = req_hour_rate
         self.last_parsed_time = time.time()
         self.lock = threading.Lock()
-        self.minute_limit = int(hour_limit / 60)
+        self.minute_rate = int(req_hour_rate / 60)
         self.delta = 0
         self.headers = headers
 
-    def req(self, url: str, params: Dict) -> Response:
+    def request_get(self, url: str, req_args: Dict) -> Response:
         with self.lock:
             self.delta += 1
             now = time.time()
             time_delta = 60 - (now - self.last_parsed_time)
             if time_delta < 60:
-                if self.delta >= self.minute_limit:
+                if self.delta >= self.minute_rate:
                     time.sleep(time_delta)
                     self.last_parsed_time = now + time_delta
                     self.delta = 0
             else:
                 self.last_parsed_time = now
                 self.delta = 0
-            return requests.get(url, params=params, headers=self.headers)
+            res = requests.get(url, params=req_args, headers=self.headers)
+            if res.status_code >= 400:
+                raise RequestError(f"error while getting resource with status code - {res.status_code}")
+            return res
 
 
 class RequestIterator:
-    index: int
-    articles: List
-    prev_size: int
-    page: int
-    inspector: RequestInspector
-
     def __init__(self, inspector: RequestInspector):
         self.index = 0
         self.inspector = inspector
         self.page = 1
-        self.prev_size = 20
+        self.prev_size = 200
         self.articles = list()
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.index == len(self.articles) and self.prev_size == 20:
-            resp = self.inspector.req(ARTICLES_REQ_URL,
-                                      {"pageNo": self.page, "pageSize": 20, "isTransform": "false", "tagId": ""})
-            self.page += 1
-            if resp.status_code < 400:
+    def __iter__(self) -> Dict:
+        for i in self.articles:
+            if self.index == len(self.articles) and self.prev_size == 200:
+                resp = self.inspector.request_get(ARTICLES_REQ_URL,
+                                                  {"pageNo": self.page, "pageSize": 200, "isTransform": "false",
+                                                   "tagId": ""})
+                self.page += 1
                 new_articles_data = resp.json().get("data").get("contents")
                 self.articles.extend(new_articles_data)
                 self.prev_size = len(new_articles_data)
-        if self.index < len(self.articles):
-            value = self.articles[self.index]
+            yield i
             self.index += 1
-            return value
-        else:
-            raise StopIteration
+
+    def __next__(self) -> Dict:
+        pass
 
 
 class RequestError(Exception):
@@ -113,25 +133,20 @@ class ContentParsingError(Exception):
     pass
 
 
-class ArticleInfo:
-    header: str
-    content: str
-    publication_dt: datetime
-    parsing_dt: datetime
-    html: str
-    href: str
-    meta_keywords: List[str]
+ArticleInfo = collections.namedtuple('ArticleInfo', ['header', 'content', 'publication_dt',
+                                                     'parsing_dt', 'html', 'href', 'meta_keywords'])
+ArticleInfoShort = collections.namedtuple('ArticleInfo', ['id', 'createTime'])
 
 
-class ArticleEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            return obj.strftime('%Y-%m-%dT%H:%M:%S%z')
-        return super().default(obj)
+# class CustomEncoder(json.JSONEncoder):
+#     def default(self, obj):
+#         if isinstance(obj, datetime):
+#             return obj.isoformat()
+#         return super().default(obj)
 
 
 def get_all_articles_binance(from_dt: Optional[datetime], to_dt: Optional[datetime], inspector: RequestInspector) -> \
-        Set[int]:
+        Set[ArticleInfoShort]:
     if from_dt is None:
         from_dt = datetime.now()
     if to_dt is None:
@@ -139,66 +154,98 @@ def get_all_articles_binance(from_dt: Optional[datetime], to_dt: Optional[dateti
     articles = set()
     from_: int = int(from_dt.timestamp() * 1000)
     to_: int = int(to_dt.timestamp() * 1000)
-    for i in RequestIterator(inspector):
-        create_time: int = i.get("createTime")
+    for article in RequestIterator(inspector):
+        create_time: int = article.get("createTime")
         if create_time > from_:
             continue
         if create_time < to_:
             break
-        articles.add(i.get('id'))
+        articles.add(ArticleInfoShort(article.get("id"), article.get("createTime") / 1000))
     return articles
 
 
-def get_article_info(href: str, inspector: RequestInspector) -> str:
-    res = inspector.req(href, {})
-    if res.status_code >= 400:
-        raise RequestError(f"error while getting resource with status code - {res.status_code}")
-    return res.text
+# def get_article_info(href: str, inspector: RequestInspector) -> str:
+#     return inspector.request_get(href, {}).text
 
 
 def parse_article_binance(html: str) -> ArticleInfo:
     bs_ = BeautifulSoup(html, "html.parser")
     try:
         article = bs_.select_one("article.css-17l2a77")
-        info = ArticleInfo()
-        info.header = article.select_one("h1.css-ps28d1").text
-        info.publication_dt = datetime.strptime(article.select_one("div.css-1hmgk20").text, "%Y-%m-%d %H:%M")
-        info.content = article.select_one("div.css-13uwx4b").text
-        info.parsing_dt = datetime.now()
-        info.html = html
-        info.href = bs_.select_one("meta[property='og:url']")["content"]
-        info.meta_keywords = bs_.select_one("meta[name='keywords']")["content"].split(', ')
+        header = article.select_one("h1.css-ps28d1").text
+        publication_dt = datetime.strptime(article.select_one("div.css-1hmgk20").text, "%Y-%m-%d %H:%M").isoformat()
+        content = article.select_one("div.css-13uwx4b").text
+        parsing_dt = datetime.now().isoformat()
+        href = bs_.select_one("meta[property='og:url']")["content"]
+        meta_keywords = bs_.select_one("meta[name='keywords']")["content"].split(', ')
+        info = ArticleInfo(header, content, publication_dt, parsing_dt, html, href, meta_keywords)
         return info
-    except TypeError as err:
+    except TypeError:
         raise ContentParsingError("some article element wasn't found")
 
 
 def save_to_disk(file_name: str, article: ArticleInfo) -> None:
-    path = pathlib.Path(f'./uploads/binance/{file_name}.zip')
-    with tempfile.NamedTemporaryFile(mode='w', suffix='.html', delete=True) as json_file, \
-            tempfile.NamedTemporaryFile(mode='w', suffix='.json', delete=True) as html_file, \
-            zipfile.ZipFile(path, 'w') as zipf:
-        html_file.write(article.html)
-        article_json = json.dumps(article.__dict__, cls=ArticleEncoder)
-        json_file.write(article_json)
-        zipf.write(json_file.name, f'{file_name}.json')
-        zipf.write(html_file.name, f'{file_name}.html')
+    path = pathlib.Path(f'./uploads/binance')
+    with tempfile.TemporaryDirectory() as temp_dir, \
+            zipfile.ZipFile(path.joinpath(f"{file_name}.zip"), 'w') as zipf:
+        with open(f"{temp_dir}/{file_name}.html", 'w') as html_file:
+            html_file.write(article.html)
+            zipf.write(html_file.name, f'{file_name}.html')
+        with open(f"{temp_dir}/{file_name}.json", 'w') as json_file:
+            article_json = json.dumps(article)
+            json_file.write(article_json)
+            zipf.write(json_file.name, f'{file_name}.json')
     return
+
+
+def find_index(conn: SqlliteConnector, from_dt: Optional[datetime], to_dt: Optional[datetime]) -> int:
+    end = conn.get_size() - 1
+    size_ = conn.get_size()
+    start = 0
+    while True:
+        middle = end - int((end - start) / 2)
+        if middle == size_:
+            return middle
+        index_date: datetime = conn.get_article_info(conn[middle])[0]
+        prev_date: datetime = conn.get_article_info(conn[middle + 1])[0]
+        if middle == 0:
+            return 0
+        if to_dt <= index_date <= from_dt < prev_date:
+            return middle
+        if index_date < from_dt:
+            start = middle
+            continue
+        if index_date > to_dt:
+            end = middle
+            continue
 
 
 def parse_articles(from_dt: Optional[datetime], to_dt: Optional[datetime]) -> None:
     inspector: RequestInspector = RequestInspector()
     conn = SqlliteConnector()
-    for article in get_all_articles_binance(from_dt, to_dt, inspector):
-        filename = f"binance_{article}"
-        if not conn.is_parsed(article):
-            try:
-                article_html = get_article_info(f"{CONCRETE_ARTICLE_URL}{article}", inspector)
-                article_info = parse_article_binance(article_html)
-                save_to_disk(filename, article_info)
-                conn.insert_article(article)
-            except Exception as ex:
-                print(ex)
+    with conn:
+        for article in get_all_articles_binance(None, None, inspector):
+            if not conn.has_article(article.id):
+                conn.insert_article(article.id, datetime.fromtimestamp(article.createTime), 0)
+            else:
+                break
+    index: int = find_index(conn, from_dt, to_dt)
+    if index != -1:
+        with conn:
+            for i in range(index):
+                article_id: int = conn[index - i]
+                if conn.get_article_info(article_id)[0] < to_dt:
+                    break
+                if conn.is_parsed(article_id):
+                    continue
+                filename = f"binance_{article_id}"
+                try:
+                    article_html = inspector.request_get(f"{CONCRETE_ARTICLE_URL}{article_id}", {}).text
+                    article_info = parse_article_binance(article_html)
+                    save_to_disk(filename, article_info)
+                    conn.set_parsed(article_id)
+                except Exception as ex:
+                    print(ex)
 
 
 if __name__ == "__main__":
